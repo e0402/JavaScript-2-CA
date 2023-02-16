5ae50aec6fa6510eb6af5e5d3fc0be43d82b479d
e0402 <100483961+e0402@users.noreply.github.com>
2023-02-16 02:12:19 +0100

    Adding posts JS code

---
+++ src/js/api/posts/create.mjs
@@ -0,0 +1,34 @@
+import { API_SOCIAL_URL } from "../constants.mjs";
+import { fetchWithToken } from "../fetchWithToken.mjs";
+
+const action = "/posts";
+const method = "post";
+
+export async function createPost(postData) {
+  const createPostURL = API_SOCIAL_URL + action;
+
+  const response = await fetchWithToken(createPostURL, {
+    method,
+    body: JSON.stringify(postData),
+  });
+
+  const post = await response.json();
+
+  console.log(post);
+}
+
+// const action = "/posts";
+// const method = "post";
+
+// export async function createPost(postData) {
+//   const createPostURL = API_SOCIAL_URL + action;
+
+//   const response = await fetchWithToken(createPostURL, {
+//     method,
+//     body: JSON.stringify(postData),
+//   });
+
+//   const post = await response.json();
+
+//   console.log(post);
+// }
